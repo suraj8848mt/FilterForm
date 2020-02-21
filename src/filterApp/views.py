4eef45319b7c0760ab8e076177144f96bd026b31
suraj.iwacademy <suraj.iwacademy@gmail.com>
2020-02-21 15:35:21 +0545

    filter by date

--- src/filterApp/views.py
+++ src/filterApp/views.py
@@ -12,7 +12,18 @@ def bootstrapfilter(request):
     title_contains_query = request.GET.get('title_contains')
     id_exact_query = request.GET.get('id_exact')
     title_or_author_query = request.GET.get('title_or_author')
-    print(title_contains_query, id_exact_query, title_or_author_query)
+    view_count_min = request.GET.get('view_count_min')
+    view_count_max = request.GET.get('view_count_max')
+    date_min = request.GET.get('date_min')
+    date_max = request.GET.get('date_max')
+    print(
+        title_contains_query,
+        id_exact_query,
+        title_or_author_query,
+        view_count_max,
+        view_count_min,
+        date_max,
+        date_min)
 
     if is_valid_queryparam(title_contains_query):
         qs = qs.filter(title__icontains=title_contains_query)
@@ -24,6 +35,18 @@ def bootstrapfilter(request):
         qs = qs.filter(Q(title__icontains=title_or_author_query)
                        | Q(author__name__icontains=title_or_author_query)
                        ).distinct()
+
+    if is_valid_queryparam(view_count_min):
+        qs = qs.filter(views__gte=view_count_min)
+
+    if is_valid_queryparam(view_count_max):
+        qs = qs.filter(views__lt=view_count_max)
+
+    if is_valid_queryparam(date_min):
+        qs = qs.filter(publish_date__gte=date_min)
+
+    if is_valid_queryparam(date_max):
+        qs = qs.filter(publish_date__lt=date_max)
 
     context = {
         'queryset': qs
